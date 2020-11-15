8a7f51828f21a8e2a5f50b0c1adf48f0b244c0d3
Hawad Ahmad <hawadjan.1@gmail.com>
2020-11-15 14:07:12 -0500

    Shows number of answered questions and mistakes

--- app/src/main/java/com/example/mathgame/ResultActivity.kt
+++ app/src/main/java/com/example/mathgame/ResultActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mathgame
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -19,16 +20,28 @@ class ResultActivity : AppCompatActivity() {
         // get and store all errors in variable
         val errors: Array<Mistake> = intent.getSerializableExtra("wrong") as Array<Mistake>
         val score = intent.getIntExtra("score", 0)
-        // val answeredQuestions = intent.getIntExtra("answered_questions", 0)
+        val timer = intent.getLongExtra("timer", 0L)
+        val answeredQuestions = intent.getIntExtra("answered_questions", 0)
 
+        // set the text of result text view
         val result = findViewById<TextView>(R.id.result_textview)
         val percentage = score * 100 / 12
         val resultText = "Score: $score/12 ($percentage%)"
         result.text = resultText
 
+        // show how many questions are answered
+        if (timer != 0L) {
+            val answeredQuestionsText = findViewById<TextView>(R.id.answeredQuestionsText)
+            answeredQuestionsText.text = getString(R.string.operation_answered, answeredQuestions)
+        }
+
 
         // if user made mistakes show them
         if (score < 12) {
+            findViewById<TextView>(R.id.mistakesText).apply {
+                visibility = View.VISIBLE
+                text = getString(R.string.mistakes, answeredQuestions-score)
+            }
             // change the adapter of the list view
             val listView = findViewById<ListView>(R.id.error_listview)
             listView.visibility = View.VISIBLE

a762bf645457cb2e6b142e8951c0e223eec3f2d6
Hawad Ahmad <hawadjan.1@gmail.com>
2020-11-15 21:34:08 -0500

    Added restart, change table and main menu buttons

--- app/src/main/java/com/example/mathgame/ResultActivity.kt
+++ app/src/main/java/com/example/mathgame/ResultActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mathgame
 
+import android.content.Intent
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
@@ -13,9 +15,25 @@ import org.w3c.dom.Text
 
 @Suppress("UNCHECKED_CAST")
 class ResultActivity : AppCompatActivity() {
+
+    private var mode = 0
+    private var timer = 0L
+    private var table = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        // get properties of game
+        mode = intent.getIntExtra("mode", 0)
+        table = intent.getIntExtra("table", 0)
+        timer = intent.getLongExtra("timer", 0L)
+
+        // set onclick listeners to buttons
+        findViewById<Button>(R.id.replayBtn).setOnClickListener { replay() }
+        findViewById<Button>(R.id.changeTableBtn).setOnClickListener { changeTable() }
+        findViewById<Button>(R.id.mainMenuBtn).setOnClickListener { mainMenu() }
 
         // get and store all errors in variable
         val errors: Array<Mistake> = intent.getSerializableExtra("wrong") as Array<Mistake>
@@ -51,5 +69,22 @@ class ResultActivity : AppCompatActivity() {
             findViewById<ImageView>(R.id.correct_image).visibility = View.VISIBLE
             findViewById<TextView>(R.id.congrats_text).visibility = View.VISIBLE
         }
+    }
+
+    private fun replay() {
+        val i = Intent(this, PlayActivity::class.java).apply {
+            putExtra("mode", mode)
+            putExtra("table", table)
+            putExtra("timer", timer)
+        }
+        startActivity(i)
+    }
+
+    private fun changeTable() {
+        startActivity(Intent(this, OptionsActivity::class.java).apply { putExtra("mode", mode) })
+    }
+
+    private fun mainMenu() {
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }

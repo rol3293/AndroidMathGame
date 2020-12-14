e054c8bc5eef181b71398178f5f8d2b7249a60f0
Hawad Ahmad <hawadjan.1@gmail.com>
2020-12-13 19:12:44 -0500

    Show mistakes, unanswered questions and correctly answered questions

--- app/src/main/java/com/example/mathgame/helpers/CustomAdapter.kt
+++ app/src/main/java/com/example/mathgame/helpers/CustomAdapter.kt
@@ -2,13 +2,18 @@ package com.example.mathgame.helpers
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.mathgame.R
 
-class CustomAdapter(private val context: Activity, private val errors: Array<Mistake>): BaseAdapter() {
+
+class CustomAdapter(private val context: Activity, private val equation: Array<Equation>) :
+    BaseAdapter() {
 
     @SuppressLint("InflateParams", "ViewHolder")
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
@@ -16,20 +21,47 @@ class CustomAdapter(private val context: Activity, private val errors: Array<Mis
         val inflater = context.layoutInflater
         val rowView = inflater.inflate(R.layout.custom_layout, null, true)
 
-        val missedEquation = rowView.findViewById<TextView>(R.id.failed_equation)
-        missedEquation.text = errors[position].getEquation()
-
-        val userAnswer = rowView.findViewById<TextView>(R.id.user_answer)
-        userAnswer.text = errors[position].getUserAnswer().toString()
+        // show the equation
+        val question = rowView.findViewById<TextView>(R.id.result_equation)
+        question.text = equation[position].getEquation()
 
         val correctAnswer = rowView.findViewById<TextView>(R.id.correct_answer)
-        correctAnswer.text = errors[position].getRightAnswer().toString()
+
+        val userAnswer = rowView.findViewById<TextView>(R.id.user_answer)
+
+        if (equation[position].answered()) {
+            userAnswer.text = equation[position].getUserAnswer()
+            if (equation[position].userIsCorrect()) {
+                userAnswer.setTextColor(Color.parseColor("#ff669900"))
+                correctAnswer.visibility = View.GONE
+                //rowView.findViewById<TextView>(R.id.textView5).layoutParams = LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1f)
+                rowView.findViewById<TextView>(R.id.textView6).visibility = View.GONE
+                rowView.findViewById<ImageView>(R.id.imageView3).visibility = View.VISIBLE
+            } else {
+                correctAnswer.text = equation[position].getRightAnswer()
+            }
+        } // Otherwise, show that it was unanswered
+        else {
+            // change the weight, size and text
+            val param1 = LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1.7f)
+            userAnswer.layoutParams = param1
+            userAnswer.textSize = 16f
+            userAnswer.text = context.getString(R.string.unanswered)
+            // hide "user answer"
+            rowView.findViewById<TextView>(R.id.textView5).visibility = View.GONE
+            // change the weight and text of textView6
+            val textView6 = rowView.findViewById<TextView>(R.id.textView6)
+            val param2 = LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1f)
+            textView6.layoutParams = param2
+            textView6.text = context.getString(R.string.answer)
+            correctAnswer.text = equation[position].getRightAnswer()
+        }
 
         return rowView
     }
 
     override fun getItem(position: Int): Any {
-        return errors[position]
+        return equation[position]
     }
 
     override fun getItemId(position: Int): Long {
@@ -37,6 +69,6 @@ class CustomAdapter(private val context: Activity, private val errors: Array<Mis
     }
 
     override fun getCount(): Int {
-        return errors.count()
+        return equation.count()
     }
 }
